e758b0f066682bae2c931916c83b2ba6c1cfc23e
sodapopinsky <nick@Nicholass-MacBook-Pro.local>
2014-12-09 17:46:12 -0600

    Initial Commit

---
+++ tastebud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tastebud
+//
+//  Created by Nicholas Spitale on 12/9/14.
+//  Copyright (c) 2014 Nicholas Spitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tastebud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tastebud
+//
+//  Created by Nicholas Spitale on 12/9/14.
+//  Copyright (c) 2014 Nicholas Spitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
